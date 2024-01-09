18738ef3678449e2dbe0be139fa950b829c83791
Haile <haile.tasma@runi.ac.il>
2024-01-09 22:46:22 +0200

    try

--- Calendar0.java
+++ Calendar0.java
@@ -6,7 +6,7 @@ public class Calendar0 {
 	
 	// Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
 	public static void main(String args[]) {
-		int year = 2000;///Integer.parseInt(args[0]);
+		int year = Integer.parseInt(args[0]);
 		isLeapYearTest(year);
 		nDaysInMonthTest(year);
 	}
